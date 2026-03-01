b4e2df258195d26bc0e631bee4cfce7da8602d90
Ahmed Fouad <tekaliker@gmail.com>
2026-03-01 03:46:29 +0200

    production

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,10 +2,10 @@ export default function HomePage() {
   return (
     <main className="bg-[url('/bg.jpg')] bg-cover bg-no-repeat">
       <div className="flex h-[92vh] flex-col items-center justify-center px-[5%] text-white backdrop-blur-xs">
-        <h2 className="mb-10 text-6xl font-bold text-shadow-md">
+        <h2 className="mb-5 text-3xl font-bold text-shadow-md md:mb-10 md:text-6xl">
           Landing Page, Welcome
         </h2>
-        <p className="text-2xl text-black">
+        <p className="text-xl text-black/70 md:text-2xl">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi
           nostrum ea excepturi nobis asperiores inventore!
         </p>
